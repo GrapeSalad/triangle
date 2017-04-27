90509aa7005c7d09eb7d02a1d0278395c68d450f
Andrew Glines and David Wilson <david@>
2017-04-27 15:11:03 -0700

    Add front-end code to create a new CSS rule that would draw a triangle with user-entered side lengths.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@
 // Scalene: No sides are equal.
 // NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.
 var triangleTest = function(side1, side2, side3) {
+  console.log(side1, side2, side3);
   if (
   ( side1 + side2 <= side3 ) ||
   ( side1 + side3 <= side2 ) ||
@@ -38,12 +39,26 @@ var triangleTest = function(side1, side2, side3) {
 //FRONT END
 $(document).ready(function(){
   $("form#triangleForm").submit(function(event){
-    var side1 = parseInt("input#side1").val();
-    var side2 = parseInt("input#side2").val();
-    var side3 = parseInt("input#side3").val();
+    var side1 = parseFloat($("input#side1").val());
+    console.log(side1);
+    var side2 = parseFloat($("input#side2").val());
+    var side3 = parseFloat($("input#side3").val());
+
     result = triangleTest(side1, side2, side3);
     event.preventDefault();
-  });
+    $("#output").text(result);
+//triangle representation???!?!?!?!?!?!!?!!?!?!?
 
+    var sheet = document.createElement('style')
+    var teststyle = "#arrow-up { padding-top: 50px; width: 0; height: 0; border-left: " + side1 + "px solid transparent; border-right: " + side2 + "px solid transparent; border-bottom: " + side3 + "px solid black;}";
+
+    // var teststyle = "#arrow-up { padding-top: 50px; width: 0; height: 0; border-left: 30px solid transparent; border-right: 50px solid transparent; border-bottom: 50px solid black;}"
+    sheet.innerHTML = teststyle;
+
+    document.body.appendChild(sheet);
+
+
+
+  });
 
 });
